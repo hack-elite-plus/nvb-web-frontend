2fde0c166ef1d6947560a4125b7142cc7c8a8410
vinod liyanage <vinodsliyanage@gmail.com>
2023-01-12 15:09:52 +0530

    add parallax provider context.

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { ParallaxProvider } from "react-scroll-parallax";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ParallaxProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ParallaxProvider>
   </React.StrictMode>
 );
 
