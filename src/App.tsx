07d0564a2392a79aa890a208ab002cd1966fef4f
vinod liyanage <vinodsliyanage@gmail.com>
2023-01-12 14:50:13 +0530

    add home route.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import { Home } from "@mui/icons-material";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+    </Routes>
   );
 }
 
