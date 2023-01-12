27cd6ba45d3d71061ab4566bb67e031545077e3f
vinod liyanage <vinodsliyanage@gmail.com>
2023-01-12 23:20:10 +0530

    fix video importing error in typescript.

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="react-scripts" />
+
+// https://stackoverflow.com/a/65851792/15084645
+// fix video importing error -  can't resolve ./assets/video.mp4 error
+
+declare module "*.mp4" {
+  const src: string;
+  export default src;
+}
