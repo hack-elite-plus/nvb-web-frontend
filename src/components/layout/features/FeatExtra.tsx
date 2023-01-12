dc45418913a731d5c806ff8c6c4b97eed8b7836b
vinod liyanage <vinodsliyanage@gmail.com>
2023-01-12 23:25:52 +0530

    fix some style issue.

--- src/components/layout/features/FeatExtra.tsx
+++ src/components/layout/features/FeatExtra.tsx
@@ -1,4 +1,5 @@
 import { Box, Stack, Typography, Paper } from "@mui/material";
+import Container from "@mui/material/Container";
 import React from "react";
 import { Parallax } from "react-scroll-parallax";
 
@@ -12,36 +13,35 @@ const extraItems = [
 
 const FeatExtra: React.FC = () => {
   return (
-    <Box sx={{ mt: 30 }}>
-      <Parallax speed={-10}>
-        <Stack
-          direction="column"
-          alignItems="center"
-          textAlign={{ md: "unset", xs: "center" }}
-          spacing={2}
-          sx={{ mb: 20 }}
-        >
-          <Typography component="h2" variant="h2">
-            More Features
-          </Typography>
-
+    <Container maxWidth="lg">
+      <Box sx={{ mt: 30 }}>
+        <Parallax speed={-10}>
           <Stack
+            direction="column"
+            alignItems="center"
+            textAlign={{ md: "unset", xs: "center" }}
             spacing={2}
-            direction={{ md: "row", xs: "column" }}
-            sx={{ pt: 4 }}
+            sx={{ mb: 20 }}
           >
-            {extraItems.map((item, index) => (
-              <Paper
-                key={index}
-                sx={{ p: 3, borderRadius: "10px" }}
-              >
-                <Typography>{item}</Typography>
-              </Paper>
-            ))}
+            <Typography component="h2" variant="h2">
+              More Features
+            </Typography>
+
+            <Stack
+              spacing={2}
+              direction={{ md: "row", xs: "column" }}
+              sx={{ pt: 4 }}
+            >
+              {extraItems.map((item, index) => (
+                <Paper key={index} sx={{ p: 3, borderRadius: "10px" }}>
+                  <Typography>{item}</Typography>
+                </Paper>
+              ))}
+            </Stack>
           </Stack>
-        </Stack>
-      </Parallax>
-    </Box>
+        </Parallax>
+      </Box>
+    </Container>
   );
 };
 
