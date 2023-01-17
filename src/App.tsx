145584acb9f533bddccfb576ca1ab54928a6684d
vinod liyanage <vinodsliyanage@gmail.com>
2023-01-17 11:42:52 +0530

    add a theme.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,55 @@
-import React from "react";
+import {
+  createTheme,
+  CssBaseline,
+  PaletteMode,
+  ThemeProvider,
+} from "@mui/material";
+import React, { useMemo } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/router/Home";
 
+const getDesignTokens = (mode: PaletteMode) => ({
+  palette: {
+    mode,
+    primary: {
+      main: "#000",
+    },
+    ...(mode === "dark" && {
+      background: {
+        default: "#19042e",
+        paper: "#18032d",
+      },
+      primary: {
+        main: "#fff",
+      },
+      text: {
+        primary: "#fff",
+        secondary: "rgba(255, 255, 255, 0.7)",
+      },
+    }),
+  },
+});
+
 function App() {
+  const [mode, setMode] = React.useState<"light" | "dark">("dark");
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+  
+  const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />}/>
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Routes>
+        <Route path="/" element={<Home toggleColorMode={colorMode.toggleColorMode}/>} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
