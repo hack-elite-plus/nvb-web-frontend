8bf9eec9fe06e98afb58aefcaace67e2f7e699ff
vinod liyanage <vinodsliyanage@gmail.com>
2023-01-17 11:32:26 +0530

    add FeatMore component.

--- src/components/layout/Features.tsx
+++ src/components/layout/Features.tsx
@@ -5,6 +5,9 @@ import FeatPet from "./features/FeatPet";
 import FeatFallDetection from "./features/FeatFallDetection";
 import FeatExtra from "./features/FeatExtra";
 import FeatGPS from "./features/FeatGPS";
+import FeatMore from "./features/FeatMore";
+
+
 
 const Features: React.FC = () => {
   return (
@@ -15,6 +18,7 @@ const Features: React.FC = () => {
       <FeatPet />
       <FeatFallDetection />
       <FeatExtra />
+      <FeatMore/>
     </>
   );
 };
