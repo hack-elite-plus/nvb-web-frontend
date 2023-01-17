db4da0cb80c94abdcf05dc7f8678eddb67217b8e
vinod liyanage <vinodsliyanage@gmail.com>
2023-01-17 11:32:00 +0530

    add a small paragraph for the body.

--- src/components/layout/features/FeatGPS.tsx
+++ src/components/layout/features/FeatGPS.tsx
@@ -15,12 +15,16 @@ const FeatGPS: React.FC = () => {
         <Box width={{ md: "50%" }}>
           <Parallax speed={-15}>
             <Stack direction="column" sx={{ p: 2, m: 3, mt: 25 }} spacing={3}>
-              <Typography component="h2" variant="h2" >
+              <Typography component="h2" variant="h2">
                 Real-time GPS tracking when you need it the most
               </Typography>
               <Typography component="h3" variant="subtitle1"></Typography>
               <Typography component="p" variant="body1">
-                
+                A GPS tracking band is a wearable device that uses GPS to track
+                location and movements. It can connect to a smartphone for
+                real-time location data and alerts. It's useful for personal
+                safety, fitness tracking, and tracking children or elderly.
+                Available in various designs and features.
               </Typography>
             </Stack>
           </Parallax>
