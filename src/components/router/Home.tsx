de9606950125d3ed5984176e433ab25ae8dbd923
vinod liyanage <vinodsliyanage@gmail.com>
2023-01-18 19:03:43 +0530

    create home component.

--- src/components/router/Home.tsx
+++ src/components/router/Home.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Features from "../layout/Features";
+import Footer from "../layout/Footer";
 import Hero from "../layout/Hero";
+import NavBar from "../layout/NavBar";
 
-const Home: React.FC = () => {
-  return <>
-    <Hero/>
-  </>
+interface HomeProps {
+  setSignInShow: React.Dispatch<React.SetStateAction<boolean>>;
+  toggleColorMode: () => void;
+}
+
+const Home: React.FC<HomeProps> = (props) => {
+  const {setSignInShow, toggleColorMode} = props;
+
+  return (
+    <>
+      <NavBar setSignInShow={setSignInShow} toggleColorMode={toggleColorMode}/>
+      <Hero />
+      <Features />
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
